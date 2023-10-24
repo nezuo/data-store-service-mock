b35d08d2c039d1768a214805374b02c857b00659
nezuo <hinckleytmicah@gmail.com>
2023-10-24 12:48:45 -0700

    Add runtime type checking for user ids

--- src/DataStoreKeyInfo.lua
+++ src/DataStoreKeyInfo.lua
@@ -2,6 +2,25 @@ local DataStoreKeyInfo = {}
 DataStoreKeyInfo.__index = DataStoreKeyInfo
 
 function DataStoreKeyInfo.new(createdTime, updatedTime, version, userIds, metadata)
+	local userIdsType = typeof(userIds)
+
+	if userIdsType ~= "nil" and userIdsType ~= "table" then
+		error(`expected userIds to be a table or nil, got {userIdsType}`)
+	end
+
+	if userIdsType == "table" then
+		local expectedKey = 1
+		for key, value in userIds do
+			assert(typeof(value) == "number", "userId must be a number")
+
+			if key ~= expectedKey then
+				error(`expected userIds to be an array, got invalid key: {key}`)
+			end
+
+			expectedKey += 1
+		end
+	end
+
 	return setmetatable({
 		CreatedTime = createdTime * 1000,
 		UpdatedTime = updatedTime * 1000,
